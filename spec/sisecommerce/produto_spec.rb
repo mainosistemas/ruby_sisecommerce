f23a98caa2f27b5f078bbfbf38064201fc4edbdf
Iuri Madeira <iurimadeira@gmail.com>
2016-06-23 22:34:26 -0300

    Fixes all get specs

--- spec/sisecommerce/produto_spec.rb
+++ spec/sisecommerce/produto_spec.rb
@@ -1,31 +1,31 @@
 RSpec.describe Sisecommerce::Produto do
   describe '#get' do
     context 'having 10 produtos available' do
-      it 'returns produto with id 1' do
-        produtos = Sisecommerce::Produto.get(id: 1)
-        expect(produtos.first.id).to eq 1
-      end
-
-      it 'returns produto with id_sku 1' do
-        produtos = Sisecommerce::Produto.get(id_sku: 1)
-        expect(produtos.first.id_sku).to eq 1
-      end
-
       it 'lists produtos available' do
         produtos = Sisecommerce::Produto.get
         expect(produtos.size).to eq 10
       end
 
-      it 'lists clientes available by pagina' do
-        clientes = Sisecommerce::Cliente.get(pagina: 1)
-        expect(clientes.size).to eq 10
+      it 'returns produto with specific id' do
+        produtos = Sisecommerce::Produto.get(id: 1)
+        expect(produtos.first.id).to eq 1
+      end
+
+      it 'returns produto with specific id_sku' do
+        produtos = Sisecommerce::Produto.get(id_sku: 1)
+        expect(produtos.first.id_sku).to eq 1
+      end
+
+      it 'lists produtos available by pagina' do
+        produtos = Sisecommerce::Produto.get(pagina: 1)
+        expect(produtos.size).to eq 10
       end
     end
 
     context 'having no produtos available' do
       it 'returns error 220' do
-        retorno = Sisecommerce::Produto.get
-        expect(retorno.first.codigo).to eq 220
+        retorno = Sisecommerce::Pedido.get
+        expect(retorno.erros.first.codigo).to eq 220
       end
     end
   end
